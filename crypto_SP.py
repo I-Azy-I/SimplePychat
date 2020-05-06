72efc8e378858d284b9ce4c315c4143f406bcbf0
JulienBerthod <julienberthod3@gmail.com>
2020-05-06 18:31:01 +0200

    Amélioration du module crypto_SP
    - Augmentation du nombre d'itérations de la fonction d'hash du mot de 
    passe pour des raisons de sécurité
    - Ajout de commentaires sur crypto_SP

--- crypto_SP.py
+++ crypto_SP.py
@@ -4,18 +4,18 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import datetime
 
 
-
-#A faire au tout début#
-def create_key(password):
+#A faire au tout début: #
+def create_key(password): #Fonction de création d'une clé de cryptage dérivée à partir du mot de passe du salon#
     password=password.encode()
-    salt=""
+    salt="" #Aucun salage car non nécessaire#
     kdf= PBKDF2HMAC(
                 algorithm=hashes.SHA256(),
-                length=32,
+                length=32, #longueur de la clé#
                 salt=salt.encode(),
-                iterations=100000,
+                iterations=500000, #nombres d'itérations de la fonction d'hashage#
                 backend=default_backend() ,
             )
     key=base64.urlsafe_b64encode(kdf.derive(password))
@@ -23,10 +23,13 @@ def create_key(password):
     return Fernet_Key
 
 
-def encrypt(Fernet_Key, message):
+
+
+#A faire à chaque fois qu'un message est envoié ou reçu#
+def encrypt(Fernet_Key, message): #Encryptage du message à l'envoi#
     return Fernet_Key.encrypt(message.encode())
 
-def decrypt(Fernet_Key, message):
+def decrypt(Fernet_Key, message): #Décryptage du message à la réception#
     try:
         data=Fernet_Key.decrypt(message)
         return data
